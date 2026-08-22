a92ddb75454deb57780e77ee811d4fbf9332aab2
timmKal01 <timkev001@gmail.com>
2026-08-22 12:13:31 +0300

    implement fixes

--- my-app/app/(Auth)/sign-up.tsx
+++ my-app/app/(Auth)/sign-up.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import React from 'react';
 import { Link } from 'expo-router';
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
