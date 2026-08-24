6213acd5dfd0358fb7c658620e4bd1054b9f9350
timmKal01 <timkev001@gmail.com>
2026-08-24 08:50:51 +0300

    Implement Fonts

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -9,48 +9,19 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className="text-xl font-bold text-success">
-        Welcome to Tech Timmy Hub!
+      <Text className="text-7xl font-sans-bold text-primary">
+        Home
       </Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        {' '}
+      <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
         Go to Onboarding
       </Link>
-      <Link
-        href="/(Auth)/sign-in"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        {' '}
+      <Link href="/(Auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
         Sign In
       </Link>
-      <Link
-        href="/(Auth)/sign-up"
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        {' '}
+      <Link href="/(Auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
         Sign Up
       </Link>
-      <Link
-        href={{
-          pathname: '/subscriptions/[id]',
-          params: { id: 'spotify' },
-        }}
-        className="mt-4 rounded bg-primary text-white p-4"
-      >
-        Spotify Subscriptions
-      </Link>
-      <Link
-        href={{
-            pathname:"/subscriptions/[id]",
-            params: {id: "claude"}
-        }}
-      >
-        {' '}
-        Your Max Subscription
-      </Link>
+
     </SafeAreaView>
   );
 }
